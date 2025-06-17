a7036a80269bac01cb26081aab97ebd7d1d23e9d
Iva Kocourkova <iva@kocourkovi.com>
2025-06-17 10:10:40 +0200

    Track if user can have another guess

--- src/game_logic.py
+++ src/game_logic.py
@@ -9,9 +9,13 @@ class WordleGame:
         self.guesses = []
 
     @property
-    def is_solved(self):
+    def is_solved(self) -> bool:
         """Game is solved if the latest guess is equal to the secret word."""
         return self.guesses[-1] == self.secret_word
 
+    @property
+    def can_guess(self) -> bool:
+        """The player has another guess if he does not run out of guesses and
+        his last guess was not correct."""
 
-
+        return not self.is_solved and len(self.guesses) < self.MAX_GUESSES
